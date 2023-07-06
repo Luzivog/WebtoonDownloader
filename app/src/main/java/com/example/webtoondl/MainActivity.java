d01eb655bf54d08add96971f206cd3c32c9d8660
Thomas Fairhurst <thpfai@gmail.com>
2023-07-05 22:59:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/webtoondl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.webtoondl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
